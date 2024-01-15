4f86f1c125147bce502330dcc28e1257a145d658
majit2006 <abbasowmajit85@gmail.com>
2024-01-15 21:21:45 +0500

    15.01.24

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let addtodobutton = document.getElementById("addToDo");
 let TodoList = document.getElementById("TodoList");
 let inputField = document.getElementById("inputField");
-
+let removebtn = document.querySelector(".remove");
 // addtodobutton.addEventListener("click", () => {
 //   let h4 = document.createElement("h4");
 //   let removebtn = document.createElement("button");
@@ -15,16 +15,19 @@ addtodobutton.addEventListener("click", () => {
   let todoItem = document.createElement("div");
   let h4 = document.createElement("h4");
   let removebtn = document.createElement("button");
-  todoItem.className = "todo-item d-flex justify-content-between";
+  todoItem.className = "todo-item d-flex justify-content-between mt-2";
   TodoList.appendChild(todoItem);
   todoItem.appendChild(h4);
   h4.innerText = inputField.value;
   todoItem.appendChild(removebtn);
-  removebtn.className = "btn btn-sm btn-danger ";
+  removebtn.className = "btn btn-sm btn-danger remove";
   removebtn.innerHTML = `
   <i
                         class="bi bi-x"></i></button>
   `;
 
   inputField.value = "";
+  removebtn.addEventListener("click", () => {
+    todoItem.remove();
+  });
 });
